8ace2fd4bc5629b687f993a97bcfbc34ce6083a1
Anshul Chaudhary <60177875+anshulgit7@users.noreply.github.com>
2021-03-05 13:39:07 +0530

    initial commit

---
+++ assets/img/BACKGROUND.JPEG_files/custom.js
@@ -0,0 +1,17 @@
+/*!
+* Themerella
+*
+* (c) Copyright themerella.com
+*
+* @version 1.0.0
+* @author  Themerella
+*/
+
+
+
+(function($) {
+    'use strict';
+})(jQuery);
+jQuery(document).ready(function(){
+jQuery('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>');
+})
